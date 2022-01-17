e33079d058e579d3ab311231588d581b4afc1817
JurajMak <93321577+JurajMak@users.noreply.github.com>
2022-01-17 13:56:13 +0100

    Završena Matrica

--- Zadatci/Matrica.java
+++ Zadatci/Matrica.java
@@ -11,17 +11,18 @@ public class Matrica {
 		int r = Integer.parseInt(JOptionPane.showInputDialog("Broj redaka"));
 		int s = Integer.parseInt(JOptionPane.showInputDialog("Broj stupaca"));
 		int[][] matrica = new int[r][s];
-
+		int sum = r * s;
 		int b = 1;
 		int r1 = r - 1;
 		int r2 = 0;
 		int s1 = s - 1;
 		int s2 = 0;
-
-		while (r2 <= r1) {
+		vanjska: while (r2 <= r1) {
 			for (int i = s1; i >= s2; i--) {
 				matrica[r1][i] = b;
 				b++;
+				if (b > sum)
+					break vanjska;
 			}
 			r1--;
 
@@ -29,6 +30,8 @@ public class Matrica {
 
 				matrica[i][s2] = b;
 				b++;
+				if (b > sum)
+					break vanjska;
 			}
 			s2++;
 
@@ -36,19 +39,25 @@ public class Matrica {
 
 				matrica[r2][i] = b;
 				b++;
+				if (b > sum)
+					break vanjska;
 			}
 			r2++;
 
 			for (int i = r2; i <= r1; i++) {
 				matrica[i][s1] = b;
 				b++;
+				if (b > sum)
+					break vanjska;
+
 			}
 			s1--;
 
 		}
-		for (int i = 0; i < matrica.length; i++) {
 
+		for (int i = 0; i < matrica.length; i++) {
 			System.out.println(Arrays.toString(matrica[i]));
 		}
+
 	}
 }
