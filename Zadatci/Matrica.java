c29b0f20847ce1ba4de7d799cffdb1f0687147f9
JurajMak <93321577+JurajMak@users.noreply.github.com>
2022-01-17 12:26:42 +0100

    Nadopunjena matrica

--- Zadatci/Matrica.java
+++ Zadatci/Matrica.java
@@ -13,33 +13,39 @@ public class Matrica {
 		int[][] matrica = new int[r][s];
 
 		int b = 1;
-		int r1 = r-1;
+		int r1 = r - 1;
 		int r2 = 0;
-		int s1 = s-1;
+		int s1 = s - 1;
 		int s2 = 0;
 
-		for (int i = s1; i > s2; i--) {
-			matrica[r1][i] = b;
-			b++;
+		while (r2 <= r1) {
+			for (int i = s1; i >= s2; i--) {
+				matrica[r1][i] = b;
+				b++;
+			}
+			r1--;
+
+			for (int i = r1; i >= r2; i--) {
+
+				matrica[i][s2] = b;
+				b++;
+			}
+			s2++;
+
+			for (int i = s2; i <= s1; i++) {
+
+				matrica[r2][i] = b;
+				b++;
+			}
+			r2++;
+
+			for (int i = r2; i <= r1; i++) {
+				matrica[i][s1] = b;
+				b++;
+			}
+			s1--;
+
 		}
-
-		for (int i = r1; i > r2; i--) {
-
-			matrica[i][s2] = b;
-			b++;
-		}
-
-		for (int i = s2; i < s1; i++) {
-
-			matrica[r2][i] = b;
-			b++;
-		}
-
-		for (int i = r2; i < r1; i++) {
-			matrica[i][s1] = b;
-			b++;
-		}
-
 		for (int i = 0; i < matrica.length; i++) {
 
 			System.out.println(Arrays.toString(matrica[i]));
